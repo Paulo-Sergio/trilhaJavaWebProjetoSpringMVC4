1e3c9a5de2c400dc8f3173521d1d0c9e2c312876
Paulo Sergio <paulofranca.contato@gmail.com>
2016-04-20 20:16:46 -0300

    aula para validação de mensagens com spring (javax.validation)

--- src/br/com/caelum/contas/controller/ContaController.java
+++ src/br/com/caelum/contas/controller/ContaController.java
@@ -2,7 +2,10 @@ package br.com.caelum.contas.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,9 +21,12 @@ public class ContaController {
 	}
 
 	@RequestMapping("/adicionaConta")
-	public String adiciona(Conta conta) {
+	public String adiciona(@Valid Conta conta, BindingResult result) {
+		if(result.hasErrors()){
+			return "formulario";
+		}
+		
 		System.out.println("Conta adicionada é: " + conta.getDescricao());
-
 		ContaDAO dao = new ContaDAO();
 		dao.adiciona(conta);
 
