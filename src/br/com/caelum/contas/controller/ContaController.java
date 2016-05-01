e966e4a6a7d5eaba7439fa755be49c1c41ef7b7a
Paulo Sergio <paulofranca.contato@gmail.com>
2016-05-01 14:55:22 -0300

    fim do curso de spring alura

--- src/br/com/caelum/contas/controller/ContaController.java
+++ src/br/com/caelum/contas/controller/ContaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,11 @@ import br.com.caelum.contas.modelo.Conta;
 @Controller
 public class ContaController {
 	
-	public ContaController() {
-		
+	private ContaDAO dao;
+
+	@Autowired
+	public ContaController(ContaDAO dao) {
+		this.dao = dao;
 	}
 
 	@RequestMapping("/form")
@@ -32,7 +36,6 @@ public class ContaController {
 		}
 		
 		System.out.println("Conta adicionada é: " + conta.getDescricao());
-		ContaDAO dao = new ContaDAO();
 		dao.adiciona(conta);
 
 		return "conta/conta-adicionada";
@@ -40,7 +43,6 @@ public class ContaController {
 
 	@RequestMapping("/listaContas")
 	public ModelAndView lista() {
-		ContaDAO dao = new ContaDAO();
 		List<Conta> contas = dao.lista();
 
 		ModelAndView mv = new ModelAndView("conta/lista");
@@ -51,7 +53,6 @@ public class ContaController {
 	
 	@RequestMapping("/removeConta")
 	public String remove(Conta conta) {
-		ContaDAO dao = new ContaDAO();
 		dao.remove(conta);
 
 		return "redirect:listaContas";
@@ -59,7 +60,6 @@ public class ContaController {
 	
 	@RequestMapping("/pagaConta")
 	public void paga(Conta conta, HttpServletResponse response){
-		ContaDAO dao = new ContaDAO();
 		dao.paga(conta.getId());
 		
 		response.setStatus(200);
